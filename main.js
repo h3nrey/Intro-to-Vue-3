158c2376220a093423448a4baae846f15b240733
h3nrey <pedro.novaes.education@gmail.com>
2022-07-26 11:19:22 -0300

    event handling, challenge: add button to remove of cart

--- main.js
+++ main.js
@@ -11,5 +11,17 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart () {
+            this.cart += 1;
+        },
+        removeToCart (){
+            if(this.cart > 0)
+            this.cart --;
+        },
+        updateImage (imageVariant) {
+            this.image = imageVariant;
+        }
     }
 })
