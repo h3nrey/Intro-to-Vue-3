783fed0a48f7d45d26505b50b0100c9cb18d632f
h3nrey <pedro.novaes.education@gmail.com>
2022-07-26 10:27:42 -0300

    list rendering, challenge: display sizes array

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id:"456578", color: "red"},
+                {id:"457867", color: "green"},
+            ],
+            sizes: ['p','m','g','gg','xg']
         }
     }
 })
