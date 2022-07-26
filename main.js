1c1e62ef206850a3b8649b1d5f158398b762e979
h3nrey <pedro.novaes.education@gmail.com>
2022-07-26 16:36:05 -0300

    updating challenge answer

--- main.js
+++ main.js
@@ -23,15 +23,16 @@ const app = Vue.createApp({
     },
     computed: {
         tittle() {
-            if(this.onSale)
-            return this.brand + " " + this.product + "is on sale "
-            else return this.brand + " " + this.product
+            return this.brand + " " + this.product
         },
         image(){
             return this.variants[this.selectedVariant].image
         },
         inStock(){
             return this.variants[this.selectedVariant].quantity
+        },
+        sale(){
+            if(this.onSale) return this.brand + " " + this.product + " is on sale"
         }
     }
 })
